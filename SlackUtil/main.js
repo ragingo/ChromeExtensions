78703e1292f2f0cd4a5b189d26d4bcc932d90bab
ragingo <save.the.world.crush@gmail.com>
2019-10-06 19:10:18 +0900

    チーム切り替え対応

--- SlackUtil/main.js
+++ SlackUtil/main.js
@@ -64,14 +64,41 @@ let state = {
 
 const loadState = () => {
   try {
-    Object.assign(state, JSON.parse(localStorage.getItem('slackutil-state')));
+    let root = {};
+    Object.assign(root, JSON.parse(localStorage.getItem('slackutil-state')));
+    Object.assign(state, root[currentTeamId()]);
   } catch {}
 };
 
+/**
+ * local storage 上の State オブジェクトの構造
+ *
+ * root
+ *   TeamID
+ *     starred
+ *       filter: string
+ *       expanded: bool
+ *     normal
+ *       filter: string
+ *       expanded: bool
+ */
 const saveState = () => {
   try {
-    localStorage.setItem('slackutil-state', JSON.stringify(state));
+    let root = {};
+    Object.assign(root, JSON.parse(localStorage.getItem('slackutil-state')));
+
+    root[currentTeamId()] = state;
+    localStorage.setItem('slackutil-state', JSON.stringify(root));
   } catch {}
+};
+
+const currentTeamId = () => {
+  try {
+    const config = JSON.parse(localStorage.getItem('localConfig_v2'));
+    return config.lastActiveTeamId;
+  } catch {
+    return '';
+  }
 };
 
 const updateChannelList = (type, channels) => {
